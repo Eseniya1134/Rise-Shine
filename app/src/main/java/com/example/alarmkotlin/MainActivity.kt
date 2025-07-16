c84a09c55db87ca79414cd53abfe6443d80488d2
eseniya1134 <shakirova.esya23@gmail.com>
2025-07-17 00:38:49 +0300

    added comment and anim

--- app/src/main/java/com/example/alarmkotlin/MainActivity.kt
+++ app/src/main/java/com/example/alarmkotlin/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.alarmkotlin
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import android.view.animation.AnimationUtils
 import androidx.core.view.WindowInsetsCompat
 import com.example.alarmkotlin.alarmList.AlarmListFragment
 import com.example.alarmkotlin.databinding.ActivityMainBinding
 import com.example.alarmkotlin.stopwatch.StopwatchFragment
 import com.example.alarmkotlin.timer.TimerFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -28,8 +31,23 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+    /**
+     * Настраивает нижнюю панель навигации
+     * Устанавливает обработчик выбора пунктов меню
+     */
     private fun setupBottomNavigation() {
+        // Устанавливаем слушатель выбора элементов в BottomNavigationView
         binding.navBottom.setOnItemSelectedListener { item ->
+            // Находим view иконки для анимации
+            val menuView = binding.navBottom.findViewById<BottomNavigationView>(R.id.nav_bottom)
+            val iconView = menuView.findViewById<View>(item.itemId)
+
+            // Запускаем анимацию
+            val animation = AnimationUtils.loadAnimation(this, R.anim.bottom_nav_anim)
+            iconView.startAnimation(animation)
+
+            // В зависимости от выбранного пункта меню заменяем фрагмент
             when (item.itemId) {
                 R.id.stopwatch-> {
                     supportFragmentManager.beginTransaction()
